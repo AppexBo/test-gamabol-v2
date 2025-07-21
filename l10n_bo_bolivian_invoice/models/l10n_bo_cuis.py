ba99db234d0ee3d933cfc325d39c6fc47c37af89
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-07-21 12:04:08 -0400

    Restaurando al codigo orignal en este archivo

--- l10n_bo_bolivian_invoice/models/l10n_bo_cuis.py
+++ l10n_bo_bolivian_invoice/models/l10n_bo_cuis.py
@@ -94,15 +94,11 @@ class l10nBoCuis(models.Model):
     
     
 
-    # def getCode(self):
-    #     if self.name:
-    #         return self.name
-    #     raise UserError('No se encontro un CUIS valido')
     def getCode(self):
-        self.ensure_one()
         if self.name:
             return self.name
-        raise UserError('No se encontró un CUIS válido')
+        raise UserError('No se encontro un CUIS valido')
+    
 
     
     
