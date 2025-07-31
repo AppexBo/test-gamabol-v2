dfe696f8695e51c9dadbe12ec3395821694b2047
unknown <erickmercadooudalova@gmail.com>
2025-07-31 10:39:08 -0400

    error de texto

--- deshabilitar_impuesto/models/pos_order_line.py
+++ deshabilitar_impuesto/models/pos_order_line.py
@@ -10,7 +10,7 @@ class PosOrderLine(models.Model):
     @api.model
     def create(self, vals):
         _logger.info("DATAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: %s", vals)
-        order = self.env['pos.order'].search([('id', '=', vals.get('orden_id'))], limit=1)
+        order = self.env['pos.order'].search([('id', '=', vals.get('order_id'))], limit=1)
         if order and order.is_invoiced:
             _logger.info("CREATING PosOrderLine with values: %s", vals)
         return super(PosOrderLine, self).create(vals)
