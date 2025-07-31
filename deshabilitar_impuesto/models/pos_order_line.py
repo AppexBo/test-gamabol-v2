d27fa01c5fe045d4b100c2ba7893a315161756e4
unknown <erickmercadooudalova@gmail.com>
2025-07-31 10:53:48 -0400

    test de valores obtenidos

--- deshabilitar_impuesto/models/pos_order_line.py
+++ deshabilitar_impuesto/models/pos_order_line.py
@@ -12,8 +12,11 @@ class PosOrderLine(models.Model):
         _logger.info("DATAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: %s", vals)
         order = self.env['pos.order'].search([('id', '=', vals.get('order_id'))], limit=1)
         _logger.info("DATAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: %s", order)
-        _logger.info("DATAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: %s", order.is_invoiced)
-        _logger.info("DATAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: %s", order.to_invoiced)
+        if order:
+            order_data = order.read()[0]  # Get all fields as a dictionary
+            self._logger.info("POS Order data: %s", order_data)
+        else:
+            self._logger.info("No POS Order found with id %s", vals.get('order_id'))
         if order and order.to_invoiced:
             _logger.info("CREATING PosOrderLine with values: %s", vals)
         return super(PosOrderLine, self).create(vals)
