4016be3c6c00c67562b4b646683c7ebe1db0c33d
GatoJustiniano <andres95liverpool@gmail.com>
2025-08-03 02:23:10 -0400

    add original

--- appex_promociones_pos/static/src/js/pos_multi_2x1.js
+++ appex_promociones_pos/static/src/js/pos_multi_2x1.js
@@ -22,13 +22,12 @@ patch(Order.prototype, {
         const totalIsZero = totalWithTax === 0;
         const globalDiscountLines = this._getGlobalDiscountLines();
         const globalDiscountPercent = globalDiscountLines.length ? this.pos.reward_by_id[globalDiscountLines[0].reward_id].discount : 0;
-
         for (const couponProgram of allCouponPrograms) {
             const program = this.pos.program_by_id[couponProgram.program_id];
             if (program.pricelist_ids.length > 0 && (!this.pricelist || !program.pricelist_ids.includes(this.pricelist.id))) {
                 continue;
             }
-            if (program.trigger === "with_code") {
+            if (program.trigger == "with_code") {
                 if (!this._canGenerateRewards(program, totalWithTax, totalWithoutTax)) {
                     continue;
                 }
@@ -36,14 +35,12 @@ patch(Order.prototype, {
             if ((coupon_id && couponProgram.coupon_id !== coupon_id) || (program_id && couponProgram.program_id !== program_id)) {
                 continue;
             }
-
             const points = this._getRealCouponPoints(couponProgram.coupon_id);
-
             for (const reward of program.rewards) {
                 if (points < reward.required_points) {
                     continue;
                 }
-                if ((reward.program_id.program_type === 'coupons' && this.orderlines.find((line) => line.reward_id === reward.id))) {
+                if ((reward.program_id.program_type === 'coupons' && this.orderlines.find(((rewardline) => rewardline.reward_id === reward.id)))) {
                     continue;
                 }
                 if (auto && this.disabledRewards.has(reward.id)) {
@@ -55,41 +52,7 @@ patch(Order.prototype, {
                 if (reward.reward_type === "discount" && totalIsZero) {
                     continue;
                 }
-
                 let unclaimedQty;
-
-                // ✅ MULTI-APLICACIÓN DE REWARDS DE TIPO DISCOUNT
-                if (reward.reward_type === "discount") {
-                    const validProductIds = new Set(
-                        reward.program_id.rules.flatMap(rule => rule.valid_product_ids || [])
-                    );
-
-                    if (!validProductIds.size) {
-                        continue;
-                    }
-
-                    const matchingQty = this.orderlines
-                        .filter(line => validProductIds.has(line.product.id))
-                        .reduce((sum, line) => sum + line.quantity, 0);
-
-                    const minQty = reward.program_id.rules[0].min_quantity || 1;
-                    const applicableTimes = Math.floor(matchingQty / minQty);
-
-                    if (applicableTimes <= 0) {
-                        continue;
-                    }
-
-                    for (let i = 0; i < applicableTimes; i++) {
-                        result.push({
-                            coupon_id: couponProgram.coupon_id,
-                            reward: reward,
-                        });
-                    }
-
-                    continue; // Saltar el push original para evitar duplicados
-                }
-
-                // Lógica original para reward_type === "product"
                 if (reward.reward_type === "product") {
                     if (!reward.multi_product) {
                         const product = this.pos.db.get_product_by_id(reward.reward_product_ids[0]);
@@ -102,7 +65,6 @@ patch(Order.prototype, {
                         continue;
                     }
                 }
-
                 result.push({
                     coupon_id: couponProgram.coupon_id,
                     reward: reward,
@@ -111,7 +73,7 @@ patch(Order.prototype, {
             }
         }
 
-        console.log('Se aplicó el descuento según la promoción')
+        console.log('Se aplicó el descuento según la promoción', result)
 
         return result;
     },
