112ddfd5892666dd771b1511e4a0d770580b4438
GatoJustiniano <andres95liverpool@gmail.com>
2025-08-03 01:21:04 -0400

    change pos a order

--- appex_promociones_pos/static/src/js/pos_multi_2x1.js
+++ appex_promociones_pos/static/src/js/pos_multi_2x1.js
@@ -3,26 +3,16 @@
 import { patch } from "@web/core/utils/patch";
 import { Order } from "@point_of_sale/app/store/models/order";
 
-// Interceptamos la búsqueda de recompensas aplicables
 patch(Order.prototype, {
-    getPotentialDiscountRewards() {
-        const rewards = super.getPotentialDiscountRewards();
+    _applyReward(reward, couponId, args) {
+        console.log("Se aplicó una recompensa:");
+        console.log("Tipo:", reward.reward_type);
+        console.log("Producto relacionado:", args.product);
+        console.log("Precio:", args.price);
+        console.log("Cantidad:", args.quantity);
+        console.log("Puntos usados:", args.cost);
 
-        if (rewards?.length) {
-            for (const reward of rewards) {
-                console.log(' Se encontró una recompensa aplicable:', reward.name);
-                console.log(' Tipo de recompensa:', reward.reward_type);
-                console.log(' Monto del descuento:', reward.discount || 0);
-
-                // Verificamos si es 2x1 basado en cantidad mínima
-                for (const rule of reward.program_id.rules) {
-                    if (rule.min_quantity >= 2) {
-                        console.log(` Regla cumplida: mínimo ${rule.min_quantity} productos`);
-                    }
-                }
-            }
-        }
-
-        return rewards;
-    },
+        // Llama a la función original
+        return super._applyReward(reward, couponId, args);
+    }
 });
