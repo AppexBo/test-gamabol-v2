2708d6fb5e4e8ed1dd93a50e10899f7f3620c6f2
GatoJustiniano <andres95liverpool@gmail.com>
2025-08-03 01:35:03 -0400

    js change

--- appex_promociones_pos/static/src/js/pos_multi_2x1.js
+++ appex_promociones_pos/static/src/js/pos_multi_2x1.js
@@ -1,7 +1,7 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import { Order } from "@point_of_sale/app/store/models/order";
+import { Order } from "@point_of_sale/app/store/models";
 
 patch(Order.prototype, {
     _applyReward(reward, couponId, args) {
