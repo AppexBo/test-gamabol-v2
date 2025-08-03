1c88369b11f487d883c43b7f62a79593bb9047a5
GatoJustiniano <andres95liverpool@gmail.com>
2025-08-03 01:04:28 -0400

    add order

--- appex_promociones_pos/static/src/js/pos_multi_2x1.js
+++ appex_promociones_pos/static/src/js/pos_multi_2x1.js
@@ -1,20 +1,28 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import { PosStore } from "@point_of_sale/app/store/pos_store";
+import { Order } from "@point_of_sale/app/store/models/order";
 
-// Interceptamos el momento en que se procesan los datos de lealtad
-patch(PosStore.prototype, {
-    _loadLoyaltyData() {
-        const result = super._loadLoyaltyData();
+// Interceptamos la búsqueda de recompensas aplicables
+patch(Order.prototype, {
+    getPotentialDiscountRewards() {
+        const rewards = super.getPotentialDiscountRewards();
 
-        // Buscamos si hay recompensas de tipo descuento
-        for (const reward of this.rewards || []) {
-            if (reward.reward_type === 'discount') {
-                console.log('Hola mundo, se aplicó el descuento');
+        if (rewards?.length) {
+            for (const reward of rewards) {
+                console.log(' Se encontró una recompensa aplicable:', reward.name);
+                console.log(' Tipo de recompensa:', reward.reward_type);
+                console.log(' Monto del descuento:', reward.discount || 0);
+
+                // Verificamos si es 2x1 basado en cantidad mínima
+                for (const rule of reward.program_id.rules) {
+                    if (rule.min_quantity >= 2) {
+                        console.log(` Regla cumplida: mínimo ${rule.min_quantity} productos`);
+                    }
+                }
             }
         }
 
-        return result;
+        return rewards;
     },
 });
