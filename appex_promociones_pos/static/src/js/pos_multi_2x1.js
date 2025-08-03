76d99fb62c76ed4b0142b0d6268caad627f5d6cb
GatoJustiniano <andres95liverpool@gmail.com>
2025-08-03 00:25:38 -0400

    test console

--- appex_promociones_pos/static/src/js/pos_multi_2x1.js
+++ appex_promociones_pos/static/src/js/pos_multi_2x1.js
@@ -1,35 +1,17 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import { Order } from "@point_of_sale/app/store/models/order";
+import { LoyaltyLoader } from "@point_of_sale/app/models/loyalty_program/loyalty_program_loader";
 
-// Interceptar descuentos por lealtad
-patch(Order.prototype, {
-    async getPotentialDiscountRewards(couponProgram) {
-        const program = this.pos.program_by_id[couponProgram.program_id];
-        const points = this.getLoyaltyPoints(program);
+// Interceptamos el momento en que se procesan los datos de lealtad
+patch(LoyaltyLoader.prototype, {
+    _loadLoyaltyData() {
+        const result = super._loadLoyaltyData();
 
-        const result = [];
-
-        for (const reward of program.rewards.filter(r => r.reward_type === 'discount')) {
-            const applicableLines = this.get_orderlines().filter(
-                (line) =>
-                    !line.is_reward_line &&
-                    (!reward.all_discount_product_ids.size || reward.all_discount_product_ids.has(line.product.id))
-            );
-
-            const subtotal = applicableLines.reduce((sum, line) => sum + line.get_base_price(), 0);
-            const discountAmount = reward.discount_type === 'percentage'
-                ? (subtotal * reward.discount) / 100
-                : reward.discount;
-
-            if (discountAmount > 0 && points >= reward.required_points) {
-                console.log("Hola mundo, se aplicó el descuento");
-                result.push({
-                    coupon_id: couponProgram.coupon_id,
-                    reward,
-                    potentialDiscount: discountAmount,
-                });
+        // Buscamos si hay recompensas de tipo descuento
+        for (const reward of this.rewards || []) {
+            if (reward.reward_type === 'discount') {
+                console.log('Hola mundo, se aplicó el descuento');
             }
         }
 
