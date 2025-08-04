54ced14c5d36f04337fb581cbac5d924ebb8cbb9
GatoJustiniano <andres95liverpool@gmail.com>
2025-08-04 11:13:21 -0400

    change model

--- appex_promociones_pos/models/loyalty_program.py
+++ appex_promociones_pos/models/loyalty_program.py
@@ -5,8 +5,7 @@ class LoyaltyProgram(models.Model):
 
     def _export_for_pos_ui(self):
         res = super()._export_for_pos_ui()
-        for reward in res['rewards']:
-            reward_id = reward.get('id')
-            reward_obj = self.env['loyalty.reward'].browse(reward_id)
+        for reward in res.get('rewards', []):
+            reward_obj = self.env['loyalty.reward'].browse(reward['id'])
             reward['apply_multiple'] = reward_obj.apply_multiple
         return res
