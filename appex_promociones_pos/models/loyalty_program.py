54f760bdf2dca5a0fb570a3d584c0ac4188aa794
GatoJustiniano <andres95liverpool@gmail.com>
2025-08-04 11:52:52 -0400

    change code

--- appex_promociones_pos/models/loyalty_program.py
+++ appex_promociones_pos/models/loyalty_program.py
@@ -1,4 +1,3 @@
-# models/loyalty_program.py
 from odoo import models
 import logging
 
@@ -8,8 +7,12 @@ class LoyaltyProgram(models.Model):
     _inherit = 'loyalty.program'
 
     def _export_for_pos_ui(self):
+        _logger.warning("***** ENTRANDO A EXPORTACIÓN DE POS *****")
         res = super()._export_for_pos_ui()
+
         for reward in res.get('rewards', []):
             reward_obj = self.env['loyalty.reward'].browse(reward['id'])
+            _logger.warning(f"Recompensa {reward_obj.id} - apply_multiple: {reward_obj.apply_multiple}")
             reward['apply_multiple'] = reward_obj.apply_multiple
+
         return res
