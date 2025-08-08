4e2aaeb9cda8474bca6e973119ed73db31a26d8d
GatoJustiniano <andres95liverpool@gmail.com>
2025-08-08 15:36:44 -0400

    fix orden posible

--- bo_invoice_custom_report/__manifest__.py
+++ bo_invoice_custom_report/__manifest__.py
@@ -5,8 +5,8 @@
     'author': 'APPEX BOLIVIA SRL.',
     'depends': ['base','account','l10n_bo_bolivian_invoice'],
     'data': [
+        'views/report_res_company.xml',
         'views/report_roll_inherit.xml',
-        'views/res_company.xml',
     ],
     'installable': True,
     'auto_install': False,
